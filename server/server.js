daac008738b50de381861bc4dfeaba5200774ad4
adib <adibhaidarzaky@gmail.com>
2024-01-11 23:42:17 +0700

    add: cros for local

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
