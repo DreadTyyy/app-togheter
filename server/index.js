7016d0101f51d9225215ac1cadc75e990c159281
adib <adibhaidarzaky@gmail.com>
2024-01-14 20:52:18 +0700

    Add: cors config index

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const path = require("path");
 require("dotenv").config();
 
 const app = express();
-app.use(cors({ origin: "https://app-togheter-lac.vercel.app/" }));
+app.use(
+  cors({
+    origin: "https://app-togheter-lac.vercel.app/",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(
